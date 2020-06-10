007eeb890974c8e41ced6a592d817309bc4e81c6
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-06-10 14:54:31 +0600

    First Final
    Solar System Simulation

---
+++ Assets/PerspectivePan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectivePan : MonoBehaviour
+{
+    private Vector3 touchStart;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //by Screen to world point we record that point
+            touchStart = GetWorldPosition(0);
+        }        
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - GetWorldPosition(0);
+            Camera.main.transform.position += direction;
+        }
+
+
+    }
+    private Vector3 GetWorldPosition(float z)
+    {
+        Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        ground.Raycast(mousePos, out distance);
+        return mousePos.GetPoint(distance);
+    }
+}
